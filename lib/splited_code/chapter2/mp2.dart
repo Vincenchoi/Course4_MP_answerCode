5c8a186109d2b52b9925a091feb3ffea83228be6
dev <swfact.dev@gmail.com>
2023-12-05 11:28:55 +0900

    [Add] 코드 아카이빙 폴더 추가

---
+++ lib/splited_code/chapter2/mp2.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+// TODO: firebase 통신 간, loading dialog 필요함
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCpY1waE7Z1C9wHHnNeTFoXcPUON4tKDTE",
+      authDomain: "course4-microproject-test.firebaseapp.com",
+      projectId: "course4-microproject-test",
+      storageBucket: "course4-microproject-test.appspot.com",
+      messagingSenderId: "4648850754",
+      appId: "1:4648850754:web:a32ea18dfa3ea566fce4db",
+    ),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/sign_up',
+      routes: {
+        '/sign_up': (context) => SignUpScreen(),
+      },
+    );
+  }
+}
+
+class SignInTextButton extends StatelessWidget {
+  const SignInTextButton({
+    super.key,
+    required this.onPressed,
+    required this.buttonName,
+    required this.color,
+  });
+
+  final Function() onPressed;
+  final String buttonName;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        minimumSize: Size.zero,
+        padding: EdgeInsets.zero,
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        buttonName,
+        style: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+          color: color,
+        ),
+      ),
+    );
+  }
+}
+
+class AppLogo extends StatelessWidget {
+  const AppLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      'https://firebasestorage.googleapis.com/v0/b/course4-microproject-test.appspot.com/o/images%2FMicrogram_logo.png?alt=media&token=bb3f7b8b-9275-47cf-9c30-22fee3069465',
+      width: 174,
+      height: 50,
+    );
+  }
+}
+
+class SignInTextField extends StatelessWidget {
+  const SignInTextField({
+    super.key,
+    required this.controller,
+    this.obscureText = false,
+    required this.hintText,
+  });
+
+  final TextEditingController controller;
+  final bool obscureText;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 268,
+      height: 38,
+      child: TextField(
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+          color: Color(0xFF737373),
+        ),
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFFDBDBDB)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFFDBDBDB)),
+          ),
+          filled: true,
+          fillColor: Color(0xFFFAFAFA),
+          hoverColor: Colors.transparent,
+          hintText: hintText,
+          hintStyle: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: Color(0xFF737373),
+          ),
+          contentPadding: EdgeInsets.all(10),
+        ),
+      ),
+    );
+  }
+}
+
+class SignInElevatedButton extends StatelessWidget {
+  const SignInElevatedButton({
+    super.key,
+    required this.onPressed,
+    required this.buttonName,
+  });
+
+  final Function() onPressed;
+  final String buttonName;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 268,
+      height: 38,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          buttonName,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  SignUpScreen({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  final AuthHelper _authHelper = AuthHelper();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AppLogo(),
+            SizedBox(height: 34),
+            SignInTextField(
+              controller: _emailController,
+              hintText: '이메일',
+            ),
+            SizedBox(height: 13),
+            SignInTextField(
+              controller: _passwordController,
+              obscureText: true,
+              hintText: '비밀번호',
+            ),
+            SizedBox(height: 34),
+            SignInElevatedButton(
+              onPressed: () async {
+                _authHelper.signUpEmailAndPassword(
+                  _emailController.text,
+                  _passwordController.text,
+                );
+                _emailController.clear();
+                _passwordController.clear();
+              },
+              buttonName: '가입',
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '계정이 있으신가요? ',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SignInTextButton(
+                  onPressed: () {},
+                  buttonName: '로그인',
+                  color: Color(0xFF3C95EF),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AuthHelper {
+  var _auth = FirebaseAuth.instance;
+
+  /// 회원가입
+  Future<bool> signUpEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
